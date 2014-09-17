bcde6db45530421009ad23af9ef6fbe881e3c342
zhanghailei <zhh5919@163.com>
2014-09-17 13:41:28 +0800

    format code use Eclipse built-in

--- src/main/java/redis/clients/jedis/async/process/AsyncDispatcher.java
+++ src/main/java/redis/clients/jedis/async/process/AsyncDispatcher.java
@@ -21,270 +21,271 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
 public class AsyncDispatcher extends Thread {
-    private static final int SELECTOR_SELECT_TIMEOUT = 300;
+	private static final int SELECTOR_SELECT_TIMEOUT = 300;
 
-    protected Logger log = Logger.getLogger(getClass().getName());
+	protected Logger log = Logger.getLogger(getClass().getName());
 
-    private Connection connection;
-    private Selector selector;
-    private AtomicBoolean shutdown = new AtomicBoolean(false);
+	private Connection connection;
+	private Selector selector;
+	private AtomicBoolean shutdown = new AtomicBoolean(false);
 
-    private final ByteBuffer readBuffer;
-    private AtomicBoolean wakeUp = new AtomicBoolean();
-    private volatile boolean  inWriteNowLoop;
-    private volatile boolean writeSuspended;
-    private Deque<AsyncJedisTask> readTaskQueue = new LinkedBlockingDeque<AsyncJedisTask>();
-    private Deque<AsyncJedisTask> writeTaskQueue = new LinkedBlockingDeque<AsyncJedisTask>();
+	private final ByteBuffer readBuffer;
+	private AtomicBoolean wakeUp = new AtomicBoolean();
+	private volatile boolean inWriteNowLoop;
+	private volatile boolean writeSuspended;
+	private Deque<AsyncJedisTask> readTaskQueue = new LinkedBlockingDeque<AsyncJedisTask>();
+	private Deque<AsyncJedisTask> writeTaskQueue = new LinkedBlockingDeque<AsyncJedisTask>();
 
-    private String password;
-    private int writeHandleLimit = 100;
+	private String password;
+	private int writeHandleLimit = 100;
 
-    static {
-        /**
-         * this is to avoid the jvm bug: NullPointerException in Selector.open()
-         * http://bugs.sun.com/view_bug.do?bug_id=6427854
-         */
-        try {
-            Selector.open().close();
-        } catch (IOException ie) {
-        }
-    }
+	static {
+		/**
+		 * this is to avoid the jvm bug: NullPointerException in Selector.open()
+		 * http://bugs.sun.com/view_bug.do?bug_id=6427854
+		 */
+		try {
+			Selector.open().close();
+		} catch (IOException ie) {
+		}
+	}
 
-    public AsyncDispatcher(Connection connection, int bufferSize)
-            throws IOException {
-        this.connection = connection;
-        connect();
+	public AsyncDispatcher(Connection connection, int bufferSize)
+			throws IOException {
+		this.connection = connection;
+		connect();
 
-        selector = Selector.open();
-        configureSocketChannelToUseSelector();
+		selector = Selector.open();
+		configureSocketChannelToUseSelector();
 
-        if (bufferSize <= 0) {
-            throw new IllegalArgumentException("Buffer bufferSize <= 0");
-        }
+		if (bufferSize <= 0) {
+			throw new IllegalArgumentException("Buffer bufferSize <= 0");
+		}
 
-        readBuffer = ByteBuffer.allocateDirect(bufferSize);
-    }
+		readBuffer = ByteBuffer.allocateDirect(bufferSize);
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-        handleAuth();
-    }
+	public void setPassword(String password) {
+		this.password = password;
+		handleAuth();
+	}
 
-    private void configureSocketChannelToUseSelector() {
-        try {
-            SocketChannel socketChannel = connection.getSocketChannel();
-            socketChannel.configureBlocking(false);
-            socketChannel.register(selector, SelectionKey.OP_READ);
-        } catch (IOException e) {
-            throw new JedisConnectionException(e);
-        }
-    }
+	private void configureSocketChannelToUseSelector() {
+		try {
+			SocketChannel socketChannel = connection.getSocketChannel();
+			socketChannel.configureBlocking(false);
+			socketChannel.register(selector, SelectionKey.OP_READ);
+		} catch (IOException e) {
+			throw new JedisConnectionException(e);
+		}
+	}
 
-    @Override
-    public void run() {
-        while ((!shutdown.get()) || writeTaskQueue.peek() != null
-                || readTaskQueue.peek() != null) {
+	@Override
+	public void run() {
+		while ((!shutdown.get()) || writeTaskQueue.peek() != null
+				|| readTaskQueue.peek() != null) {
 
-            try {
-                interestOpWriteIfAnyRequestPending();
-                wakeUp.set(false);
-                int num = selector.select(SELECTOR_SELECT_TIMEOUT);
-                if (num < 0) {
-                    continue;
-                }
-                Set selectedKeys = selector.selectedKeys();
-                Iterator it = selectedKeys.iterator();
-                while (it.hasNext()) {
-                    SelectionKey key = (SelectionKey) it.next();
+			try {
+				interestOpWriteIfAnyRequestPending();
+				wakeUp.set(false);
+				int num = selector.select(SELECTOR_SELECT_TIMEOUT);
+				if (num < 0) {
+					continue;
+				}
+				Set selectedKeys = selector.selectedKeys();
+				Iterator it = selectedKeys.iterator();
+				while (it.hasNext()) {
+					SelectionKey key = (SelectionKey) it.next();
 
-                    if (key.isReadable()) {
-                        handleRead(key);
-                    }
+					if (key.isReadable()) {
+						handleRead(key);
+					}
 
-                    if (key.isValid() && key.isWritable()) {
-                        SocketChannel sc = (SocketChannel) key.channel();
-                        handleWrite(sc);
-                    }
+					if (key.isValid() && key.isWritable()) {
+						SocketChannel sc = (SocketChannel) key.channel();
+						handleWrite(sc);
+					}
 
-                    it.remove();
-                }
-            } catch (IOException e) {
-                handleConnectionException(new JedisConnectionException(e));
-            } catch (JedisConnectionException e) {
-                handleConnectionException(e);
-            }
-            if (wakeUp.get())
-                selector.wakeup();
-        }
-    }
+					it.remove();
+				}
+			} catch (IOException e) {
+				handleConnectionException(new JedisConnectionException(e));
+			} catch (JedisConnectionException e) {
+				handleConnectionException(e);
+			}
+			if (wakeUp.get())
+				selector.wakeup();
+		}
+	}
 
-    public synchronized void registerRequest(AsyncJedisTask task) {
-        if(writeSuspended || inWriteNowLoop){
-            writeTaskQueue.add(task);
-        }else{
-            if(!_write(connection.getSocketChannel(), task)){
-                writeTaskQueue.addFirst(task);
-            }
-        }
-        
-        if (wakeUp.compareAndSet(false, true)) {
-            selector.wakeup();
-        }
-    }
+	public synchronized void registerRequest(AsyncJedisTask task) {
+		if (writeSuspended || inWriteNowLoop) {
+			writeTaskQueue.add(task);
+		} else {
+			if (!_write(connection.getSocketChannel(), task)) {
+				writeTaskQueue.addFirst(task);
+			}
+		}
 
-    public void setShutdown(boolean shutdown) {
-        this.shutdown.set(shutdown);
-    }
+		if (wakeUp.compareAndSet(false, true)) {
+			selector.wakeup();
+		}
+	}
 
-    private synchronized void handleConnectionException(
-            JedisConnectionException e) {
-        while (readTaskQueue.peek() != null) {
-            AsyncJedisTask task = readTaskQueue.poll();
-            task.getCallback().execute(null, e);
-        }
+	public void setShutdown(boolean shutdown) {
+		this.shutdown.set(shutdown);
+	}
 
-        // discard connection
-        disconnect();
+	private synchronized void handleConnectionException(
+			JedisConnectionException e) {
+		while (readTaskQueue.peek() != null) {
+			AsyncJedisTask task = readTaskQueue.poll();
+			task.getCallback().execute(null, e);
+		}
 
-        // reconnect
-        while (!shutdown.get() && !connection.isConnected()) {
-            try {
-                connection.connect();
-                configureSocketChannelToUseSelector();
-                handleAuth();
-            } catch (JedisConnectionException e1) {
-                try {
-                    log.warning("Connection failed to Redis "
-                            + connection.getHost() + ":" + connection.getPort()
-                            + ", sleeping and retry...");
-                    Thread.sleep(1000);
-                } catch (InterruptedException e2) {
-                }
-            }
-        }
+		// discard connection
+		disconnect();
 
-        if (shutdown.get()) {
-            while (writeTaskQueue.peek() != null) {
-                AsyncJedisTask task = writeTaskQueue.poll();
-                task.getCallback().execute(null, e);
-            }
-        }
-    }
+		// reconnect
+		while (!shutdown.get() && !connection.isConnected()) {
+			try {
+				connection.connect();
+				configureSocketChannelToUseSelector();
+				handleAuth();
+			} catch (JedisConnectionException e1) {
+				try {
+					log.warning("Connection failed to Redis "
+							+ connection.getHost() + ":" + connection.getPort()
+							+ ", sleeping and retry...");
+					Thread.sleep(1000);
+				} catch (InterruptedException e2) {
+				}
+			}
+		}
 
-    private void handleAuth() {
-        if (password != null) {
-            writeTaskQueue.addFirst(buildAuthRequest());
-        }
-    }
+		if (shutdown.get()) {
+			while (writeTaskQueue.peek() != null) {
+				AsyncJedisTask task = writeTaskQueue.poll();
+				task.getCallback().execute(null, e);
+			}
+		}
+	}
 
-    private AsyncJedisTask buildAuthRequest() {
-        return new AsyncJedisTask(RequestBuilder.build(Protocol.Command.AUTH,
-                password), new AsyncResponseCallback<String>() {
-            @Override
-            public void execute(String response, JedisException exc) {
-                //
-            }
-        });
-    }
+	private void handleAuth() {
+		if (password != null) {
+			writeTaskQueue.addFirst(buildAuthRequest());
+		}
+	}
 
-    private synchronized boolean _write(SocketChannel sc,AsyncJedisTask task) {
-        
-        int writtenLen = 0;
-        boolean isFinish = false;
-        while(task.getWriteBuf().hasRemaining()){         
-                try {
-                    writtenLen = sc.write(task.getWriteBuf());
-                } catch (IOException e) {
-                    throw new JedisConnectionException("occuors an error while write on channel", e);
-                }
-                if(writtenLen == 0)
-                    break;
-        }
+	private AsyncJedisTask buildAuthRequest() {
+		return new AsyncJedisTask(RequestBuilder.build(Protocol.Command.AUTH,
+				password), new AsyncResponseCallback<String>() {
+			@Override
+			public void execute(String response, JedisException exc) {
+				//
+			}
+		});
+	}
 
-        if (task.isWriteComplete()) {
-            readTaskQueue.add(task);
-            writeSuspended = false;
-            isFinish = true;
-        }else{
-            writeSuspended = true;
-        }
-        
-        return isFinish;
-    }
+	private synchronized boolean _write(SocketChannel sc, AsyncJedisTask task) {
 
-    private void handleWrite(SocketChannel sc) throws IOException {
-        // System.out.println("handler write");
-     
-        int completeCount = 0;
-        AsyncJedisTask task = writeTaskQueue.peek();
-        inWriteNowLoop = true;
-        while (task != null && completeCount < writeHandleLimit) {
-             _write(sc, task);           
-        }
-        inWriteNowLoop = false;
-        
-    }
+		int writtenLen = 0;
+		boolean isFinish = false;
+		while (task.getWriteBuf().hasRemaining()) {
+			try {
+				writtenLen = sc.write(task.getWriteBuf());
+			} catch (IOException e) {
+				throw new JedisConnectionException(
+						"occuors an error while write on channel", e);
+			}
+			if (writtenLen == 0)
+				break;
+		}
 
-    private void handleRead(SelectionKey key) throws IOException {
-        // System.out.println("handler Read");
-        int numOfBytes;
-        do {
-            numOfBytes = readDataFromSocket(key);
-            if (numOfBytes == -1) {
-                key.cancel();
-                throw new JedisConnectionException(
-                        "It seems like server has closed the connection.");
-            } else if (numOfBytes > 0) {
-                AsyncJedisTask task = readTaskQueue.peek();
-                while (readBuffer.hasRemaining()) {
-                    if (task == null) {
-                        throw new JedisConnectionException(
-                                "Remaining received data but no remaining request");
-                    }
+		if (task.isWriteComplete()) {
+			readTaskQueue.add(task);
+			writeSuspended = false;
+			isFinish = true;
+		} else {
+			writeSuspended = true;
+		}
 
-                    task.appendPartialResponse(readBuffer.get());
-                    if (task.isReadComplete()) {
-                        // TODO : Thread Pool?
-                        task.callback();
+		return isFinish;
+	}
 
-                        readTaskQueue.poll();
-                        task = readTaskQueue.peek();
-                    }
-                }
-            }
-        } while (numOfBytes > 0);
-    }
+	private void handleWrite(SocketChannel sc) throws IOException {
+		// System.out.println("handler write");
 
-    private int readDataFromSocket(SelectionKey key) throws IOException {
-        // Read the data
-        SocketChannel sc = (SocketChannel) key.channel();
+		int completeCount = 0;
+		AsyncJedisTask task = writeTaskQueue.peek();
+		inWriteNowLoop = true;
+		while (task != null && completeCount < writeHandleLimit) {
+			_write(sc, task);
+		}
+		inWriteNowLoop = false;
 
-        readBuffer.clear();
-        int numOfBytes = sc.read(readBuffer);
-        readBuffer.flip();
+	}
 
-        return numOfBytes;
-    }
+	private void handleRead(SelectionKey key) throws IOException {
+		// System.out.println("handler Read");
+		int numOfBytes;
+		do {
+			numOfBytes = readDataFromSocket(key);
+			if (numOfBytes == -1) {
+				key.cancel();
+				throw new JedisConnectionException(
+						"It seems like server has closed the connection.");
+			} else if (numOfBytes > 0) {
+				AsyncJedisTask task = readTaskQueue.peek();
+				while (readBuffer.hasRemaining()) {
+					if (task == null) {
+						throw new JedisConnectionException(
+								"Remaining received data but no remaining request");
+					}
 
-    private void interestOpWriteIfAnyRequestPending()
-            throws ClosedChannelException {
-        if (writeTaskQueue.peek() != null) {
-            // we're interest of write
-            connection.getSocketChannel().register(selector,
-                    SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-        }
-    }
+					task.appendPartialResponse(readBuffer.get());
+					if (task.isReadComplete()) {
+						// TODO : Thread Pool?
+						task.callback();
 
-    private void connect() {
-        if (!connection.isConnected()) {
-            connection.connect();
-        }
-    }
+						readTaskQueue.poll();
+						task = readTaskQueue.peek();
+					}
+				}
+			}
+		} while (numOfBytes > 0);
+	}
 
-    public void disconnect() {
-        if (connection.isConnected()) {
-            connection.disconnect();
-        }
-    }
+	private int readDataFromSocket(SelectionKey key) throws IOException {
+		// Read the data
+		SocketChannel sc = (SocketChannel) key.channel();
+
+		readBuffer.clear();
+		int numOfBytes = sc.read(readBuffer);
+		readBuffer.flip();
+
+		return numOfBytes;
+	}
+
+	private void interestOpWriteIfAnyRequestPending()
+			throws ClosedChannelException {
+		if (writeTaskQueue.peek() != null) {
+			// we're interest of write
+			connection.getSocketChannel().register(selector,
+					SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+		}
+	}
+
+	private void connect() {
+		if (!connection.isConnected()) {
+			connection.connect();
+		}
+	}
+
+	public void disconnect() {
+		if (connection.isConnected()) {
+			connection.disconnect();
+		}
+	}
 
 }
--- src/main/java/redis/clients/jedis/async/process/AsyncJedisTask.java
+++ src/main/java/redis/clients/jedis/async/process/AsyncJedisTask.java
@@ -8,73 +8,74 @@ import redis.clients.jedis.async.response.BasicResponseBuilder;
 import redis.clients.jedis.exceptions.JedisException;
 
 public class AsyncJedisTask {
-    private final ByteBuffer writeBuf;
-    private final AsyncResponseCallback callback;
-    private BasicResponseBuilder responseBuilder;
-    private Builder responseTypeConverter;
+	private final ByteBuffer writeBuf;
+	private final AsyncResponseCallback callback;
+	private BasicResponseBuilder responseBuilder;
+	private Builder responseTypeConverter;
 
-    public AsyncJedisTask(byte[] request, AsyncResponseCallback callback) {
-	     this.writeBuf = ByteBuffer.wrap(request);
-	     this.callback = callback;
-    }
-
-    public AsyncJedisTask(byte[] request, Builder responseTypeConverter,
-	    AsyncResponseCallback callback) {
-         this(request, callback);
-	     this.responseTypeConverter = responseTypeConverter;
-    }
-
-    public ByteBuffer getWriteBuf() {
-	return this.writeBuf;
-    }
-
-    public Builder getResponseTypeConverter() {
-	return responseTypeConverter;
-    }
-
-    public AsyncResponseCallback getCallback() {
-	return callback;
-    }
-
-    public <T> void initializeResponseBuilder() {
-	responseBuilder = new BasicResponseBuilder<T>();
-    }
-
-    public void appendPartialResponse(byte b) {
-	getResponseBuilder().appendPartialResponse(b);
-    }
-
-    public boolean isWriteComplete() {
-        
-        return !writeBuf.hasRemaining();
-    }
-    public boolean isReadComplete() {
-	return getResponseBuilder().isComplete();
-    }
-
-    public void callback() {
-	BasicResponseBuilder responseBuilder = getResponseBuilder();
-	Builder responseTypeConverter = getResponseTypeConverter();
-	Object response = responseBuilder.getResponse();
-	JedisException exception = responseBuilder.getException();
-
-	try {
-	    if (responseTypeConverter != null && exception == null) {
-		response = responseTypeConverter.build(response);
-	    }
-
-	    getCallback().execute(response, exception);
-	} catch (Exception e) {
-	    getCallback().execute(null, new JedisException(e));
+	public AsyncJedisTask(byte[] request, AsyncResponseCallback callback) {
+		this.writeBuf = ByteBuffer.wrap(request);
+		this.callback = callback;
 	}
 
-    }
-
-    public BasicResponseBuilder getResponseBuilder() {
-	if (responseBuilder == null) {
-	    initializeResponseBuilder();
+	public AsyncJedisTask(byte[] request, Builder responseTypeConverter,
+			AsyncResponseCallback callback) {
+		this(request, callback);
+		this.responseTypeConverter = responseTypeConverter;
 	}
 
-	return responseBuilder;
-    }
+	public ByteBuffer getWriteBuf() {
+		return this.writeBuf;
+	}
+
+	public Builder getResponseTypeConverter() {
+		return responseTypeConverter;
+	}
+
+	public AsyncResponseCallback getCallback() {
+		return callback;
+	}
+
+	public <T> void initializeResponseBuilder() {
+		responseBuilder = new BasicResponseBuilder<T>();
+	}
+
+	public void appendPartialResponse(byte b) {
+		getResponseBuilder().appendPartialResponse(b);
+	}
+
+	public boolean isWriteComplete() {
+
+		return !writeBuf.hasRemaining();
+	}
+
+	public boolean isReadComplete() {
+		return getResponseBuilder().isComplete();
+	}
+
+	public void callback() {
+		BasicResponseBuilder responseBuilder = getResponseBuilder();
+		Builder responseTypeConverter = getResponseTypeConverter();
+		Object response = responseBuilder.getResponse();
+		JedisException exception = responseBuilder.getException();
+
+		try {
+			if (responseTypeConverter != null && exception == null) {
+				response = responseTypeConverter.build(response);
+			}
+
+			getCallback().execute(response, exception);
+		} catch (Exception e) {
+			getCallback().execute(null, new JedisException(e));
+		}
+
+	}
+
+	public BasicResponseBuilder getResponseBuilder() {
+		if (responseBuilder == null) {
+			initializeResponseBuilder();
+		}
+
+		return responseBuilder;
+	}
 }
